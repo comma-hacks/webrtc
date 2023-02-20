6557f2beeea96aff5be9513f4221ec2e3ebf4589
Keyvan Fatehi <keyvanfatehi@gmail.com>
2023-02-20 15:15:16 -0800

    update run script

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-export CGO_LDFLAGS="-L/home/body/go-astiav/tmp/n4.4.1/tmp/n4.4.1/lib/"
-export CGO_CXXFLAGS="-I/home/body/go-astiav/tmp/n4.4.1/tmp/n4.4.1/include/"
-export PKG_CONFIG_PATH="/home/body/go-astiav/tmp/n4.4.1/tmp/n4.4.1/lib/pkgconfig"
+export CGO_LDFLAGS="-L/home/body/webrtc-body/go-astiav/tmp/n4.4.1/lib/"
+export CGO_CXXFLAGS="-I/home/body/webrtc-body/go-astiav/tmp/n4.4.1/include/"
+export PKG_CONFIG_PATH="/home/body/webrtc-body/go-astiav/tmp/n4.4.1/lib/pkgconfig"
 go run .
